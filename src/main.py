56168c09a865415202cdcfcece45cdda2778635d
RetroMalloc <segfault.studio0@gmail.com>
2025-08-09 20:07:26 -0300

    Adicionando imagens de fundo

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ pygame.init()
 
 display_surface = pygame.display.set_mode((1280, 720))
 
+background_surface = pygame.image.load("assets/background.png").convert_alpha()
+stall_surface = pygame.image.load("assets/stall.png").convert_alpha()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -11,5 +14,9 @@ while True:
             sys.exit()
     
     display_surface.fill('white')
+
+    display_surface.blit(background_surface)
+
+    display_surface.blit(stall_surface)
 
     pygame.display.update()
